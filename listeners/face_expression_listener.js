c199c7048c80478f3b8df1bf4b8f75d275808e0a
Ruben Baena Perez <ruben.baena@gm.uca.es>
2024-07-27 16:07:41 +0000

    Update face_expression_listener.js

--- listeners/face_expression_listener.js
+++ listeners/face_expression_listener.js
@@ -6,16 +6,11 @@ window.addEventListener("message", function (event) {
         return;
     if (event.data.type && (event.data.type == "INIT_FACE_RECOGNITION")) {
 
-
-        //console.log("INIT_FACE_RECOGNITION");
-
         Promise.all(
             [
                 faceapi.nets.tinyFaceDetector.loadFromUri(event.data.msg.models),
-                //faceapi.nets.faceLandmark68Net.loadFromUri(event.data.msg.models),
                 faceapi.nets.faceRecognitionNet.loadFromUri(event.data.msg.models),
                 faceapi.nets.faceExpressionNet.loadFromUri(event.data.msg.models),
-                //faceapi.nets.ageGenderNet.loadFromUri(event.data.msg.models),
 
             ]
         ).then(
@@ -29,28 +24,19 @@ window.addEventListener("message", function (event) {
     if (event.data.type && (event.data.type == "IMAGE_CAPTURE_READY")) {
 
 
-        //console.log("IMAGE_CAPTURE_READY");
-
-     
-
         window.captureFaceExpression = async function captureFaceExpression() {
 
             try {
         
                 return window.imageCapture.takePhoto().then(async function (blob) {
-                    //console.log('Took photo:', blob);
             
                     img = document.createElement("img");
                     img.src = URL.createObjectURL(blob);
-            
-                    //document.body.appendChild(img);
-            
+                        
                     const detections = await faceapi.detectAllFaces(img, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
             
                     return detections;
-            
-                    //console.log(detections);
-            
+                        
                 }).catch(function (error) {
                     console.error('takePhoto() error: ', error);
                 });
@@ -80,8 +66,6 @@ window.addEventListener("message", function (event) {
                 expressions.sort((a, b) => b.probability - a.probability);
 
                 console.debug(expressions[0]);
-
-                //window.expressions.push(expressions[0].expression);
 
                 window.expressions.push(expressions[0]);
                 
